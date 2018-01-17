70d5b5a7be0d16019cbdb1cc3246f573d05db0e9
MARINA AZEVEDO FERREIRA SOARES <38781912862@jpmorgan.sp>
2018-01-17 21:23:07 -0200

    qtt connect

--- index.js
+++ index.js
@@ -1,54 +1,71 @@
 //import { start } from "repl";
 
 // obter o código da biblioteca
-const five = require("johnny-five");
-
+var five = require("johnny-five");
+var mqtt = require("mqtt")
 // criar uma instância que representa nossa placa
-const board = new five.Board({
+var board = new five.Board({
   port: "COM5" // substitua pela porta a qual seu Arduino estiver conectado
 });
 
-// vincular código a ser rodado quando a comunicação com a placa começar
-board.on("ready", function() {
 
-  // vincular uma variável do tipo Led que controla o pino 13
-  const led = new five.Led(13);
-  console.log("Placa pronta");
-
-
-  var portaoAberto;
-  var ledV = new five.Led(4);
-  var button = new five.Button(2);
-  var servo = new five.Servo({
-      pin: 3,
-      startAt: 19
-  });
-
-  button.on("down", function () {
-    if (portaoAberto){
-        servo.to(19, 1 * 1000);
-        portaoAberto = false;
-        ledV.on();
-        console.log("Portao fechado");
-    }else{
-        servo.to(90, 1 * 1000);
-        portaoAberto = true;
-        ledV.off();
-        console.log("Portao Aberto");
-    };
+  board.on("ready", function() {
+    //board do jfive pronta
+      console.log("Placa pronta");
     
-  });
+      
+        var portaoAberto;
+        var ledV = new five.Led(4);
+        var button = new five.Button(2);
+        var servo = new five.Servo({
+            pin: 3,
+            startAt: 19
+        });
 
-  button.on("up", function(){
-    console.log("Up");
-  });
+        button.on("down", function () {
+          if (portaoAberto){
+              servo.to(19, 1 * 1000);
+              portaoAberto = false;
+              ledV.off();
+              console.log("Portao fechado");
+          }else{
+              servo.to(90, 1 * 1000);
+              portaoAberto = true;
+              ledV.on();
+              console.log("Portao Aberto");
+          };
+          
+        });
 
-  button.on("hold", function(){
-    console.log("Hold");
-  })
+        button.on("up", function(){
+          console.log("Up");
+        });
 
+        button.on("hold", function(){
+          console.log("Hold");
+        });
 
-  // piscar o led a cada 500ms
-  led.blink(500);
+        var client = mqtt.connect("mqtt://test.mosquitto.org");
+
+        client.on("connect", function(){
+          console.log("mqtt connectado");
+  
+          client.subscribe('codexp1-portao');
+          function callback(topic, payload){
+            // 1 para fechar o portao
+            //0 para fechar
+            var msg = payload.toString();
+            if (msg === "1"){
+              abrirPortao();
+            }else {
+              console.log(topic);
+              console.log(msg);
+            }
+          }
+          client.on("message", callback);
+// vincular código a ser rodado quando a comunicação com a placa começar
+
+    });
+
 });
 
